03009f9f18d374aac010449febd9a3c8d5614a22
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-12-05 03:53:56 +0300

    sidebar-finish

---
+++ sidebar.js
@@ -0,0 +1,20 @@
+function sidebarFunc() {
+    //! home sidebar start
+    const btnOpenSidebar = document.querySelector("#btn-menu")
+    const sidebar = document.querySelector(".sidebar-left ")
+    const btnCloseSidebar = document.querySelector("#close-sidebar")
+    const collapse = document.querySelector(".collapse")
+    //? click outside start
+    btnOpenSidebar.addEventListener(("click"), () => {
+        sidebar.style.left = "0"
+        sidebar.style.visibility = "visible"
+        collapse.style.display = "block"
+        btnCloseSidebar.classList.add("close-sidebar")
+    })
+    btnCloseSidebar.addEventListener(("click"), () => {
+        sidebar.style.left = "-50%"
+        btnCloseSidebar.classList.remove("close-sidebar")
+
+    })
+}
+sidebarFunc()
