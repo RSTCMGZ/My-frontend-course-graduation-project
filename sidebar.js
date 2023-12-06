ac2c24a64e3ef1885d4cb51767b5acb23d550ea7
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-12-07 01:24:47 +0300

    index-responsive-finish

--- sidebar.js
+++ sidebar.js
@@ -10,11 +10,19 @@ function sidebarFunc() {
         sidebar.style.visibility = "visible"
         collapse.style.display = "block"
         btnCloseSidebar.classList.add("close-sidebar")
+
     })
     btnCloseSidebar.addEventListener(("click"), () => {
         sidebar.style.left = "-50%"
         btnCloseSidebar.classList.remove("close-sidebar")
 
     })
+    document.addEventListener(("click"), (e) => {
+        if (!e.composedPath().includes(sidebar) && !e.composedPath().includes(btnOpenSidebar)) {
+            sidebar.style.left = "-50%"
+            btnCloseSidebar.classList.remove("close-sidebar")
+        }
+    })
 }
+
 sidebarFunc()
