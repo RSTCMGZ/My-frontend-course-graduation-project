0401aac15b0b8009dc0f8363824fcf0080f1a4a1
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-12-15 03:27:27 +0300

    fast-cargo-cart-total

--- basket.js
+++ basket.js
@@ -1,8 +1,7 @@
 import { cart } from "./shopping.js"
 
 function displayBasketProduct() {
-    const cartWrapper = document.querySelector(".cart-wrapper")
-    const basket = document.querySelector(".basket")
+    const basket = document.querySelector(".cart-wrapper1")
     let results = ""
     cart.forEach((item) => {
         results += `
@@ -11,20 +10,18 @@ function displayBasketProduct() {
         <td class="cart-image">
           <img
             src="img/categories/cat-1.png"
-            class="w-25"
+            class="img-fluid"
+
             alt=""
           />
           <i class="bi bi-x delete-cart text-black"></i>
         </td>
         <td class="text-black">${item.name}</td>
-        <td>$80 $</td>
-        <td class="product-quantity">1</td>
+        <td>${item.price.toFixed(2)}$</td>
         <td class="product-subtotal">$80</td>
       </tr>
-        
         `
         basket ? basket.innerHTML = results : ""
-
     })
 }
 
--- cart.js
+++ cart.js
@@ -1,12 +1,15 @@
-import { cart } from "./shopping.js"
+let cart = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : []
+
 
 
 
 function displayCartProduct() {
-    const cartWrapper = document.querySelector(".cart-wrapper")
-    let results = ""
-    cart.forEach((item) => {
-        results += `
+  const cartWrapper = document.querySelector(".cart-wrapper")
+  let result = ""
+  cart.forEach((item) => {
+    result += `
         <tr class="cart-item">
         <td></td>
         <td class="cart-image">
@@ -15,18 +18,61 @@ function displayCartProduct() {
             class="img-fluid"
             alt=""
           />
-          <i class="bi bi-x delete-cart"></i>
+          <i class="bi bi-x delete-cart" data-id="${item.id}"></i>
         </td>
         <td>${item.name}</td>
-        <td>${item.price} $</td>
-        <td class="product-quantity">1</td>
-        <td class="product-subtotal">$80</td>
+        <td>$${item.price.toFixed(2)} </td>
+        <td class="product-quantity">${item.quantity}</td>
+        <td class="product-subtotal">$${(item.price * item.quantity).toFixed(2)}</td>
       </tr>
-        
         `
-        cartWrapper ? cartWrapper.innerHTML = results : ""
-
-    })
+    cartWrapper.innerHTML = result
+    removeCartItem()
+  })
 }
 
 displayCartProduct()
+
+function removeCartItem() {
+  let countItems = document.querySelector('.count')
+  const btnDeleteCart = document.querySelectorAll(".delete-cart")
+
+  btnDeleteCart.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const id = e.target.dataset.id;
+      cart = cart.filter((item) => item.id !== Number(id))
+      displayCartProduct()
+      localStorage.setItem("cart", JSON.stringify(cart))
+      countItems.innerHTML = cart.length
+      saveCartValues()
+    });
+  });
+}
+
+function saveCartValues() {
+  const cartTotal = document.getElementById("cart-total")
+  const subTotal = document.getElementById("sub-total")
+  const fastCargo = document.getElementById("fast-cargo")
+  const fastCargoPrice = 15;
+
+  let itemsTotal = 0;
+
+  cart.length > 0 && cart.map((item) => itemsTotal += item.price * item.quantity)
+  console.log(itemsTotal);
+  subTotal.innerHTML = `$${itemsTotal.toFixed(2)}`
+  cartTotal.innerHTML = `$${itemsTotal.toFixed(2)}`
+
+  fastCargo.addEventListener("change", function (e) {
+    if (e.target.checked) {
+      cartTotal.innerHTML = `$${(itemsTotal + fastCargoPrice).toFixed(2)}`
+    } else {
+      cartTotal.innerHTML = `$${itemsTotal.toFixed(2)}`
+
+    }
+  })
+
+}
+
+saveCartValues()
+
+
